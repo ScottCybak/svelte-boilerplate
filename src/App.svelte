f01eafaf10ec7a3447e2334f90389a4c6b4ea079
Scott Cybak <scybak@auxiliumgroup.com>
2021-01-19 15:37:36 -0500

    initial commit

---
+++ src/App.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	
+	import { Router, Route } from 'svelte-routing';
+	import Home from './components/Home.svelte';
+
+	let url = '';
+</script>
+
+<router-outlet>
+	<Router url={url}>
+		<Route component={Home}></Route>
+	</Router>
+</router-outlet>
---
+++ src/components/Home.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+	import { links } from 'svelte-routing';
+</script>
+
+<div>
+	Home Works!
+</div>
